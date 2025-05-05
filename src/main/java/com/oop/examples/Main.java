8cd85d096fd4ee3edab3d8856ece3675b5e14806
Santiago Ariel <143102582+santiagoarielv98@users.noreply.github.com>
2025-05-05 20:52:13 -0300

    Update Main.java

--- src/main/java/com/oop/examples/Main.java
+++ src/main/java/com/oop/examples/Main.java
@@ -33,6 +33,7 @@ public class Main {
     Person person1 = new Person("Jorge Gomez", 30, "jorge@example.com");
     Person person2 = new Person("Ana Garcia", 25, "ana@example.com");
     Person person3 = new Person("Raul Garcia", 0, "raul@example.com");
+    Person person3 = new Person("Cosme Fulanito", 33, "raul@example.com");
     List<Person> originalMembers = new ArrayList<>(Arrays.asList(person1, person2, person3));
     Familiy family = new Familiy(originalMembers);
     System.out.println(family.getMembers());
