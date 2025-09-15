f8339567e76fb9e0f98130edc564384e0e62550e
Agustin Arias <aarias@pulsarml.com>
2025-09-15 20:10:36 -0300

    A lot of refactor, moving things around and examples for patterns

---
+++ src/main/java/com/crui/patterns/structural/composite/Folders.java
@@ -0,0 +1,22 @@
+package com.crui.patterns.structural.composite;
+
+import java.util.List;
+
+public class Folders {
+
+  public static void main(String[] args) {}
+
+  class Folder {
+    private String name;
+    private List<Folder> folders;
+    private List<File> files;
+
+    public Folder(String name) {
+      this.name = name;
+    }
+  }
+
+  class File {
+    private String name;
+  }
+}
