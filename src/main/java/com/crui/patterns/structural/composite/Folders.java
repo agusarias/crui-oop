4d5e528d803d565f5b02595a8bd5b2ffba02d090
daynosolro <daynosolro@gmail.com>
2025-09-23 21:47:51 -0300

    trabajo en progreso

---
+++ src/main/java/com/crui/patterns/structural/composite/Folders.java
@@ -0,0 +1,30 @@
+package com.crui.patterns.structural.composite;
+
+import java.util.List;
+
+public class Folders {
+
+  public static void main(String[] args) {}
+
+  class Folder {
+    private String name;
+    private List<Folder> folders;
+    private List<File> files;
+
+    public Folder(String name) {
+      this.name = name;
+    }
+
+    public String getName() {
+      return this.name;
+    }
+
+    public List<File> getFiles() {
+      return this.files;
+    }
+  }
+
+  class File {
+    private String name;
+  }
+}
