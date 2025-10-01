adcbf293b7592a88a8549c95698d1f1ebe9e5db3
jazzzz0 <148421374+jazzzz0@users.noreply.github.com>
2025-09-30 21:48:16 -0300

    parcial: entrega de examen con patrones de diseño

--- src/main/java/com/crui/patterns/examples/turnos_medicos/TurnosMedicos.java
+++ src/main/java/com/crui/patterns/examples/turnos_medicos/TurnosMedicos.java
@@ -6,13 +6,29 @@ import java.util.List;
  * Turnos Medicos
  *
  * <p>Contestar a continuación las siguientes preguntas:
- *
+ * 
  * <p>- Qué patrón de diseño podés identificar en el código dado?
- *
+ *    Singleton: se utiliza para garantizar que haya una sola instancia de Database en el sistema,
+ *               esto asegura un punto global de conexión con Database que es un recurso compartido para todo el sistema.
+ *               Va a evitar problemas de inconsistencia y duplicación.
+ * 
+ *    Factory: se utiliza en CreadorDeDoctores para crear instancias de Doctor con especialidades preconfigurados,
+ *             implementarlo hace que el código no esté acoplado, evita errores y centraliza la creación de doctores.
+ * 
  * <p>- Qué patrones de diseño se podrían agregar para mejorar el código?
+ *    Strategy: para definir el precio total del turno según los descuentos por obra social y especialidad,
+ *              también se puede utilizar para definir el precio base.
+ *              
+ *    Observer: para dar avisos sobre Turno a cualquier clase suscriptora cuando cambie su estado.
+ * 
+ *    Factory: a partir de la implementación de Strategy, se abre la posibilidad de usar este patrón
+ *             para crear la estrategia apropiada según los criterios apropiados para cada Strategy,
+ *             por un lado, la especialidad del doctor, y por el otro, la obra social del paciente.
  *
  * <p>Implementar uno o más de estos patrones adicionales para mejorar el código.
- */
+ *      Implemento ambos Strategy
+ *      Aplico Factory para la creación de las instancias de Strategy
+ */   
 public class TurnosMedicos {
 
   public static void main(String[] args) {
@@ -31,47 +47,15 @@ public class TurnosMedicos {
       return;
     }
 
-    // Precio base en base a la especialidad
-    float precioBase;
-    if (doctor.especialidad.contiene("Cardiología")) {
-      precioBase = 8000;
-    } else if (doctor.especialidad.contiene("Neumonología")) {
-      precioBase = 7000;
-    } else if (doctor.especialidad.contiene("Kinesiología")) {
-      precioBase = 7000;
-    } else {
-      precioBase = 5000;
-    }
+    PrecioEspecialidad precioBase = PrecioEspecialidadFactory.crear(doctor);
 
-    // Descuento en base a la obra social y la especialidad
-    float descuento;
-    switch (paciente.obraSocial) {
-      case "OSDE":
-        descuento =
-            doctor.especialidad.contiene("Cardiología")
-                ? 1f // 100% de descuento en cardiología
-                : 0.2f; // 20% de descuento
-        break;
-      case "IOMA":
-        descuento =
-            doctor.especialidad.contiene("Kinesiología")
-                ? 1f // 100% de descuento en kinesiología
-                : 0.15f; // 15% de descuento
-        break;
-      case "PAMI":
-        descuento = 1.0f; // 100% de descuento
-        break;
-      default:
-        descuento = 0.0f; // 0% de descuento
-        break;
-    }
-
-    // Aplico el descuento
-    float precio = precioBase - precioBase * descuento;
+    Descuento descuento = DescuentoFactory.crear(paciente.obraSocial);
 
     // Nuevo turno
-    Turno turno = new Turno(paciente, doctor, "2025-01-01 10:00", precio);
+    Turno turno = new Turno(paciente, doctor, "2025-01-01 10:00", precioBase);
     System.out.println(turno);
+
+    turno.setDescuento(descuento);
 
     // Cambio de turno
     turno.setFechaYHora("2025-01-01 11:00");
@@ -153,13 +137,22 @@ public class TurnosMedicos {
     private Paciente paciente;
     private Doctor doctor;
     private String fechaYHora;
-    private double precio;
+    private PrecioEspecialidad precioEspecialidad;
+    private Descuento descuento;
+    private float precioFinal;
+  
 
-    public Turno(Paciente paciente, Doctor doctor, String fechaYHora, double precio) {
+    public Turno(Paciente paciente, Doctor doctor, String fechaYHora, PrecioEspecialidad precio) {
       this.paciente = paciente;
       this.doctor = doctor;
       this.fechaYHora = fechaYHora;
-      this.precio = precio;
+      this.precioEspecialidad = precio;
+      this.precioFinal = precio.obtener();
+    }
+
+    public void setDescuento(Descuento descuento){
+      this.descuento = descuento;
+      aplicarDescuento();
     }
 
     public void setFechaYHora(String fechaYHora) {
@@ -174,7 +167,111 @@ public class TurnosMedicos {
 
     @Override
     public String toString() {
-      return "Turno para " + paciente + " con " + doctor + " el " + fechaYHora + " - $" + precio;
+      return "Turno para " + paciente + " con " + doctor + " el " + fechaYHora + " - $" + precioFinal;
+    }
+
+  public void aplicarDescuento(){
+      if(descuento != null){
+          float valor = descuento.calcularValor(this);
+          precioFinal = this.precioEspecialidad.obtener() * (1 - valor);
+      }
+    } 
+  }
+
+  public static class PrecioEspecialidadFactory {
+    public static PrecioEspecialidad crear(Doctor doctor){
+      if (doctor.especialidad.contiene("Cardiología")) {
+        return new PrecioCardiologia();
+      } else if (doctor.especialidad.contiene("Neumonología")) {
+        return new PrecioNeumonologia();
+      } else if (doctor.especialidad.contiene("Kinesiología")) {
+        return new PrecioKinesiologia();
+      } else {
+        return new PrecioDefault();
+      }
+    }
+  }
+
+  interface PrecioEspecialidad {
+    public float obtener();
+  }
+
+  public static class PrecioCardiologia implements PrecioEspecialidad{
+    @Override
+    public float obtener(){
+      return 8000;
+    }
+  }
+
+  public static class PrecioNeumonologia implements PrecioEspecialidad{
+    @Override
+    public float obtener(){
+      return 7000;
+    }
+  }
+
+  public static class PrecioKinesiologia implements PrecioEspecialidad{
+    @Override
+    public float obtener(){
+      return 7000;
+    }
+  }
+
+  public static class PrecioDefault implements PrecioEspecialidad{
+    @Override
+    public float obtener(){
+      return 5000;
+    }
+  }
+
+  public static class DescuentoFactory{
+    public static Descuento crear(String obraSocial){
+      switch (obraSocial) {
+      case "OSDE": 
+        return new DescuentoOSDE();
+
+      case "IOMA":
+        return new DescuentoIOMA();
+
+      case "PAMI":
+        return new DescuentoPAMI(); 
+
+      default:
+        return new DescuentoDefault(); 
+      }
+    }
+  }
+  interface Descuento {
+    public float calcularValor(Turno turno);
+  }
+
+  public static class DescuentoOSDE implements Descuento {
+
+    @Override
+    public float calcularValor(Turno turno){
+          return turno.doctor.especialidad.contiene("Cardiología") ? 1f : 0.2f; // 20% de descuento
+    }
+
+  }
+
+  public static class DescuentoIOMA implements Descuento {
+    @Override
+    public float calcularValor(Turno turno){
+          return turno.doctor.especialidad.contiene("Kinesiología") ? 1f : 0.15f; 
+    }
+  }
+
+  public static class DescuentoPAMI implements Descuento {
+    @Override
+    public float calcularValor(Turno turno){
+          return 1f;
+    }
+  }
+
+  public static class DescuentoDefault implements Descuento {
+    @Override
+    public float calcularValor(Turno turno){
+          return 0f;
     }
   }
 
@@ -192,6 +289,7 @@ public class TurnosMedicos {
               CreadorDeDoctores.crearTraumatologo("Dr. Mario Gómez", "mgomez@gmail.com"));
     }
 
+    // TODO: Se encuentra singleton
     public static Database getInstance() {
       if (instance == null) {
         instance = new Database();
@@ -212,6 +310,7 @@ public class TurnosMedicos {
       return null;
     }
   }
+
 
   public static class CreadorDeDoctores {
 
